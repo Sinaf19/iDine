9a86a6757ca5bee00534176cb24044bf3a89d763
Sinaf19 <quentinsurdez@gmail.com>
2024-01-14 07:25:12 +0100

    Initial Commit

---
+++ iDine/iDineApp.swift
@@ -0,0 +1,17 @@
+//
+//  iDineApp.swift
+//  iDine
+//
+//  Created by Quentin Surdez on 14.01.24.
+//
+
+import SwiftUI
+
+@main
+struct iDineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
